40020444c48034928245f24b051f95cb2e2d011d
henriquemota <mota.henrique@gmail.com>
2023-05-29 08:33:01 -0300

    inicializacao do banco de dados

--- Palavras/src/palavras/boundary/Descobrir.java
+++ Palavras/src/palavras/boundary/Descobrir.java
@@ -1,13 +1,16 @@
 package palavras.boundary;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Descobrir extends Base {
 	
-	JTextField txt;
-	JButton btn;
-	JLabel lbl;
+	private String palavra = "";
+	private JTextField txt;
+	private JButton btn;
+	private JLabel lbl;
+	private JPanel content, panel1, panel2, panel3;
 
 	public Descobrir(String palavra) {
 		super("Descobrir");
@@ -16,37 +19,63 @@ public class Descobrir extends Base {
 		this.setSize(400,400);
 		this.setVisible(true);
 		
-		JPanel content = (JPanel)this.getContentPane();
-		content.setLayout(new BorderLayout());
+		this.palavra = palavra;
+		
+		this.content = (JPanel)this.getContentPane();
+		this.content.setLayout(new BorderLayout());
 
 		// painel 1
-		JPanel panel1 = new JPanel(new FlowLayout());
+		this.panel1 = new JPanel(new FlowLayout());
 		char[] arr = new char[palavra.length()];
 		for (int i = 0; i < palavra.length(); i++) {
-			txt = new JTextField(2);
-			txt.setEnabled(false);
-			panel1.add(txt);			
+			txt = new JTextField(4);
+			this.panel1.add(txt);			
 		}
 		
 		// painel 2
-		JPanel panel2 = new JPanel(new FlowLayout());
+		this.panel2 = new JPanel(new FlowLayout());
 		txt = new JTextField(5);
 		btn = new JButton("Ok");
-		panel2.add(txt);
-		panel2.add(btn);
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Component[] components = panel1.getComponents();
+				for (int i = 0; i < palavra.length(); i++) {
+					char letra = txt.getText().charAt(0);
+					if (palavra.charAt(i) == letra) {
+						((JTextField)components[i]).setText(String.valueOf(letra));
+						txt.setText("");
+					}
+				}				
+			}
+		});
+		
+		this.panel2.add(txt);
+		this.panel2.add(btn);
 		
 		// painel 3
-		JPanel panel3 = new JPanel(new GridLayout(0,2));
+		this.panel3 = new JPanel(new GridLayout(0,2));
 		lbl = new JLabel("Erros: 0");
-		panel3.add(lbl);
+		this.panel3.add(lbl);
 		lbl = new JLabel("Palavras");
-		panel3.add(lbl);
-		
-		
+		this.panel3.add(lbl);
+				
 		// inclui os paineis
-		content.add(panel1, BorderLayout.NORTH);
-		content.add(panel2, BorderLayout.CENTER);
-		content.add(panel3, BorderLayout.SOUTH);
-
+		this.content.add(this.panel1, BorderLayout.NORTH);
+		this.content.add(this.panel2, BorderLayout.CENTER);
+		this.content.add(this.panel3, BorderLayout.SOUTH);
 	}
+	
+	private void verificarPalavra(char letra) {
+		Component[] components = this.panel1.getComponents();
+		for (int i = 0; i < this.palavra.length(); i++) {
+			if (this.palavra.charAt(i) == letra) {
+				((JTextField)components[i]).setText(String.valueOf(letra));;
+			}
+		}
+	}
+
+
+
+	
 }
